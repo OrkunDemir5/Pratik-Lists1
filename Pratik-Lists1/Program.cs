e8e59373efca94b292c41b1ca8335efab1f4dc28
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-31 15:03:34 +0300

    Pratik-Lists1

---
+++ Pratik-Lists1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+// Davetliler listesi oluşturuldu
+List<String> isimler = new List<string>() { "Bülent Ersoy ", "Ajda Pekkan ", "Ebru Gündeş", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın" };
+
+// Bu değişken, isimleri yazdırırken her bir isme sıralama numarası vermek için kullanılacak.
+int siralama = 1;
+
+// isimler listesindeki her bir öğe için döngü başlatılıyor.
+foreach (string isim in isimler)
+{
+    // Konsola sırayla sıralama numarası ve isim yazdırılıyor.
+    Console.WriteLine($"{siralama} " + isim);
+    siralama++;
+}
+
+Console.ReadKey();
